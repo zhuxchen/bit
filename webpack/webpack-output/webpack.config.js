e5e899141eada05e4608f9494a2a33529d241d08
zhuxing.chen <zhuxing.chen@bytedance.com>
2018-10-30 19:15:41 +0800

    更新目录

--- webpack/webpack-output/webpack.config.js
+++ webpack/webpack-output/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  // 默认使用当前目录，但是推荐在配置中传递一个值。
+  // 这使得你的配置独立于 CWD(current working directory - 当前执行路径)。
+  context: path.resolve(__dirname), 
   entry: {
     app: './src/index.js',
     vendors: './src/vendors.js',
